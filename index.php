afe24b9147e15cd58c46528a7092bb0a0fef3ba7
amnoaiOCI <32835926+amnoaiOCI@users.noreply.github.com>
2019-12-17 13:39:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php 
 //header('Content-Type: application/json');
 $request = file_get_contents('php://input');
-$req_dump = print_r( $request, true );
-//$fp = file_put_contents( 'request.log', $req_dump );
-//echo $request;
+/*$req_dump = print_r( $request, true );
+$fp = file_put_contents( 'request.log', $req_dump );*/
+$text = $request;
+$events = json_decode($request,true);
+if (!is_null($events['events'])) { 
 echo 'Good...';
+}
+else
+  echo 'No event request';
 ?>
