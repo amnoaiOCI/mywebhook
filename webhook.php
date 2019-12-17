0a5ad03a3686869d9d15bff7c3c303aa06ab4eb8
amnoaiOCI <32835926+amnoaiOCI@users.noreply.github.com>
2019-12-17 11:05:39 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,4 +3,5 @@ header('Content-Type: application/json');
 $request = file_get_contents('php://input');
 $req_dump = print_r( $request, true );
 //$fp = file_put_contents( 'request.log', $req_dump );
+echo 'Good...';
 ?>
