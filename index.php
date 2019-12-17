da4e7a6b04061a8f8556f4c111cb77d23f72f64c
amnoaiOCI <32835926+amnoaiOCI@users.noreply.github.com>
2019-12-17 11:14:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ header('Content-Type: application/json');
 $request = file_get_contents('php://input');
 $req_dump = print_r( $request, true );
 //$fp = file_put_contents( 'request.log', $req_dump );
-echo $req_dump;
+echo $request;
 echo 'Good...';
 ?>
